40a16983da5c598f757d3327997a194a4d60a23a
HaRo0 <34543900+HaRo0@users.noreply.github.com>
2026-01-30 17:02:11 +0100

    Initial commit

---
+++ src/main/java/net/haro0/hytale/graveprotocol/GraveProtocol.java
@@ -0,0 +1,19 @@
+package net.haro0.hytale.graveprotocol;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import lombok.SneakyThrows;
+
+public class GraveProtocol extends JavaPlugin {
+
+    public GraveProtocol(JavaPluginInit init) {
+
+        super(init);
+    }
+
+    @Override
+    @SneakyThrows
+    protected void setup() {
+
+    }
+}
